cbe8ed957f3c353324e1643adb7b06f097b26429
Rajasegar <rajasegarc@freshdesk.com>
2019-08-07 12:44:28 +0530

    [CHORE] Getting ast and transformed output
    1. Adding Rewriter class
    2. Handling codemirror for transform editor
    3. Removing prism.js

--- app/controllers/ast_controller.rb
+++ app/controllers/ast_controller.rb
@@ -1,4 +1,13 @@
 require 'parser/current'
+class RemoveDo < Parser::TreeRewriter
+  def on_while(node)
+    # Check if the statement starts with "do"
+    if node.location.begin.is?('do')
+      remove(node.location.begin)
+    end
+  end
+end
+
 class AstController < ApplicationController
   def index
     @ast = Parser::CurrentRuby.parse("2 + 2")
@@ -12,8 +21,25 @@ class AstController < ApplicationController
     p params
     @ast = Parser::CurrentRuby.parse(params[:code])
     p @ast
+
+    code = <<-EOF
+                      while true do
+                         puts 'hello'
+                         end
+    EOF
+
+    buffer        = Parser::Source::Buffer.new('(example)')
+    buffer.source = params[:code]
+    temp = Parser::CurrentRuby.parse(params[:code])
+    rewriter      = RemoveDo.new
+
+    # Rewrite the AST, returns a String with the new form.
+    output =  rewriter.rewrite(buffer, temp)
+    puts output
+
+
     respond_to do |format|
-      format.json { render :json => { ast: @ast.to_s } } 
+      format.json { render :json => { ast: @ast.to_s, output: output.to_s } } 
     end
 
   end
