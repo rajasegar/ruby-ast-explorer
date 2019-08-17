762f9179e5915337d469f6f80be615cb5cc63e4e
Rajasegar Chandran <rajasegar.c@gmail.com>
2019-08-17 08:38:48 +0530

    [CHORE] Adding eval to apply transform methods

--- app/controllers/ast_controller.rb
+++ app/controllers/ast_controller.rb
@@ -1,12 +1,4 @@
 require 'parser/current'
-class RemoveDo < Parser::TreeRewriter
-  def on_while(node)
-    # Check if the statement starts with "do"
-    if node.location.begin.is?('do')
-      remove(node.location.begin)
-    end
-  end
-end
 
 class AstController < ApplicationController
   def index
@@ -20,13 +12,17 @@ class AstController < ApplicationController
   def create
     p params
     ast = Parser::CurrentRuby.parse(params[:code])
-    p ast
+
+    # Doing eval is not that safe, need to sanitize
+    eval(params[:transform])
+    pp ast
 
 
     buffer        = Parser::Source::Buffer.new('(example)')
     buffer.source = params[:code]
     temp = Parser::CurrentRuby.parse(params[:code])
-    rewriter      = RemoveDo.new
+    rewriter      = Transform.new
+
 
     # Rewrite the AST, returns a String with the new form.
     output =  rewriter.rewrite(buffer, temp)
